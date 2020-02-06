4f2e1b68c0d6fb2f751fc4be1ddb883964881971
Jose Nunes <josenunes34@gmail.com>
2020-02-06 14:07:59 +0000

    feat(Task Menu): New option - Schedule tasks
    WDY-266

---
+++ frontend/src/actions/tasks/scheduleTask.actions.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { call, put, takeLatest } from 'redux-saga/effects';
+import { Toaster } from '@binarycapsule/ui-capsules';
+import { addTaskAtIndex, removeTask } from '../sections';
+import { storeSelectedTaskId } from '.';
+
+export const SCHEDULE_TASK_REQUEST = 'taskActions/SCHEDULE_TASK_REQUEST';
+
+export const scheduleTask = payload => ({
+  type: SCHEDULE_TASK_REQUEST,
+  payload,
+});
+
+const scheduleTaskApi = params => axios.post('/api/tasks/schedule', params);
+
+export function* scheduleTaskSaga(action) {
+  const { dayId, sectionId, taskId, taskIndex } = action.payload;
+
+  // Optimistically remove the task from the corresponding section
+  yield put(removeTask(sectionId, taskIndex));
+  yield put(storeSelectedTaskId(''));
+
+  try {
+    yield call(() => scheduleTaskApi({ dayId, sectionId, taskId }));
+  } catch (error) {
+    Toaster.error({
+      title: 'Something went wrong',
+      message: 'Task was not scheduled',
+    });
+
+    // Add the task back in case of failure
+    yield put(addTaskAtIndex(sectionId, taskIndex, taskId));
+  }
+}
+
+export function* watchScheduleTaskSaga() {
+  yield takeLatest(SCHEDULE_TASK_REQUEST, scheduleTaskSaga);
+}
